39f3fee830434261c70c8d9b2f9846eff787d74a
ShahidC0de <shahidzada.cs@gmail.com>
2026-01-02 17:56:43 +0400

    refactoring the code

---
+++ lib/core/theme/app_theme.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class AppPallete {
+  static const Color backgroundColor = Colors.white;
+  static const Color buttonColor = Colors.red;
+  static const Color textColor = Colors.black;
+  static const Color borderColor = Colors.grey;
+  static const Color fieldTextColor = Colors.grey;
+  static const TextStyle headingText = TextStyle(
+    color: textColor,
+    fontSize: 24,
+    fontWeight: FontWeight.bold,
+  );
+  static const TextStyle subHeadingText = TextStyle(
+    color: Colors.black,
+    fontSize: 15,
+    fontWeight: FontWeight.bold,
+  );
+}
